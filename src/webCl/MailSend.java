fd011e36a74041539bf8b9b6d90ce33e8d057658
hhk <996896116@qq.com>
2017-10-26 15:11:26 +0800

    1026 successfully send emails

--- src/webCl/MailSend.java
+++ src/webCl/MailSend.java
@@ -1,4 +1,4 @@
-package tst;
+package webCl;
 
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -14,7 +14,7 @@ import java.util.Properties;
  * JDK 版本: JDK 1.7 以上（必须）
  */
 
-public class Main {
+public class MailSend {
 
     // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）, 
@@ -72,7 +72,21 @@ public class Main {
         out.flush();
         out.close();
     }
+    public static void sendMail(String receiveAdd, String ctt) throws Exception {
+    	Properties props = new Properties();                    // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+        Session session = Session.getDefaultInstance(props);
+        session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
 
+        MimeMessage message = MailSend.createMimeMessage(session, myEmailAccount, receiveAdd, ctt);
+        Transport transport = session.getTransport();
+        
+        transport.connect(myEmailAccount, myEmailPassword);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
     public static void main(String[] args) throws Exception {
         // 1. 创建参数配置, 用于连接邮件服务器的参数配置
         Properties props = new Properties();                    // 参数配置
@@ -99,7 +113,7 @@ public class Main {
         session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
 
         // 3. 创建一封邮件
-        MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount);
+        MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount, "content");
 
         // 4. 根据 Session 获取邮件传输对象
         Transport transport = session.getTransport();
@@ -136,21 +150,21 @@ public class Main {
      * @return
      * @throws Exception
      */
-    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail, String ctt) throws Exception {
         // 1. 创建一封邮件
         MimeMessage message = new MimeMessage(session);
 
         // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
-        message.setFrom(new InternetAddress(sendMail, "buui", "UTF-8"));
+        message.setFrom(new InternetAddress(sendMail, "网页知", "UTF-8"));
 
         // 3. To: 收件人（可以增加多个收件人、抄送、密送）
         message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "hfds", "UTF-8"));
 
         // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
-        message.setSubject("tit", "UTF-8");
+        message.setSubject("网页更新消息", "UTF-8");
 
         // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
-        message.setContent("veyenguilajiyzjm?", "text/html;charset=UTF-8");
+        message.setContent(ctt, "text/html;charset=UTF-8");
 
         // 6. 设置发件时间
         message.setSentDate(new Date());
---
+++ src/webCl/Main.java
@@ -0,0 +1,115 @@
+package webCl;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+public class Main {
+	public static void textWrite(final String fileName, final String cont) {
+		try {
+			File fileText = new File(fileName);
+			FileWriter fwr = new FileWriter(fileText);
+			fwr.write(cont);
+			fwr.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static String filterUtf8mb4(String str) {
+        final int LAST_BMP = 0xFFFF;
+        StringBuilder sb = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            int codePoint = str.codePointAt(i);
+            if (codePoint < LAST_BMP) {
+                sb.appendCodePoint(codePoint);
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+	public static void saveCtt(String url) {
+		String doUrl;
+		if (url.substring(0, 7).equals("http://")) {
+			doUrl = url.substring(7);
+		} else if (url.substring(0, 8).equals("https://")) {
+			doUrl = url.substring(8);
+		} else {
+			doUrl = url;
+		}
+		String[] ana = doUrl.split("/");
+		System.out.println(ana.length);
+		if (ana[0].equals("tieba.baidu.com")) {
+			System.out.println(ana[1]);
+			BaiduTieba aaa = new BaiduTieba(ana[2]);
+			aaa.saveCtt();
+			
+			
+		}
+	}
+	public static void crawlWeb() {
+		String sql = "select * from urlsdb.mails order by url";
+		Connection conn = SqlCon.con();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);  
+			String lastUrl = "xx";
+			String url;
+			while (rs.next()) {
+				url = rs.getString("url");
+				if (!url.equals(lastUrl)) {
+					saveCtt(url);
+					lastUrl = url;
+				}
+			}
+			rs.close(); 
+			conn.close(); 
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void sendMail() {
+		String sql = "select * from urlsdb.mails order by mail";
+		Connection conn = SqlCon.con();
+		User curSendUser = null;
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);  
+			String lastMail = "hh996896116@163.com";
+			String mail;
+			while (rs.next()) {
+				mail = rs.getString("mail");
+				if (!mail.equals(lastMail)) {
+					if (curSendUser != null) {
+						curSendUser.send();
+					}
+					curSendUser = new User(mail);
+					lastMail = mail;
+				}
+				curSendUser.addCtt(rs.getString("url"));
+			}
+			if (curSendUser != null) {
+				curSendUser.send();
+			}
+			rs.close(); 
+			conn.close(); 
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void myGo() {
+		crawlWeb();
+		sendMail();
+	}
+	public static void main(String[] args) {
+		myGo();
+
+		System.out.println("main finish");
+	}
+}
