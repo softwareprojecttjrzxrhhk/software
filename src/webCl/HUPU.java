1c80db962aba449d609dafdc9a28897ef36b8cb0
tian1152010110 <2282550468@qq.com>
2017-12-10 16:40:11 +0800

    12101640

--- src/webCl/HUPU.java
+++ src/webCl/HUPU.java
@@ -5,7 +5,7 @@ public class HUPU {
     return WebC.httpRequest("http://bbs.csdn.net/topics/392284846?page=1");
   }
   public static void main(String[] args) {
-    String s=htmlS("1",1); // sdd
+    String s=htmlS("1",1);
     Main.textWrite("html1.txt", s);
   }
 
